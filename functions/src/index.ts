4df7ba1387e1674fe314ac8e8e0497a2ac2d464d
Eung Jin Lee <oiojin831@gmail.com>
2019-03-27 07:14:01 +0000

    fix airbnb

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -73,7 +73,7 @@ admin.initializeApp()
 
 const firebaseDb = admin.firestore()
 
-export const addReservations = functions.https.onRequest(
+export const newAirbnb = functions.https.onRequest(
   async (request, response) => {
     const data = request.body
 
@@ -88,36 +88,18 @@ export const addReservations = functions.https.onRequest(
         .collection('reservations')
         .doc(uniqueId)
         .set({
-          platform: data.platform || 'airbnb',
+          platform: 'airbnb',
           reservationCode: data.code,
           checkInDate: data.start_date,
           checkOutDate: data.end_date,
           nights: data.nights,
           guests: data.guests,
-          status: data.status,
-          listing: data.listing,
-          stayingDates: stayingDates,
           roomNumber: airbnbRoom(data.listing.id),
-          bill: {
-            currency: data.currency,
-            securityPrice: data.security_price,
-            securityPriceFormatted: data.security_price_formatted,
-            basePrice: data.base_price,
-            basePriceFormatted: data.base_price_formatted,
-            guestFee: data.guest_fee,
-            guestFeeFormatted: data.guest_fee_formatted,
-            taxAmount: data.tax_amount,
-            taxAmountFormatted: data.tax_amount_formatted,
-            extrasPrice: data.extras_price,
-            extrasPriceFormatted: data.extras_price_formatted,
-            subtotal: data.subtotal,
-            subtotalFormatted: data.subtotal_formatted,
-            totalPrice: data.total_price,
-            totalPiceFormatted: data.total_price_formatted,
-            perNightPrice: data.per_night_price,
-            perNightPriceFormatted: data.per_night_price_formatted,
-            payoutPrice: data.payout_price,
-          },
+          price: data.total_price_formatted,
+          payoutPrice: data.payout_price,
+          phoneNumber: data.guest.phone,
+          guestName: data.guest.first_name + ' ' + data.guest.last_name,
+          stayingDates: stayingDates,
         })
       response.status(200).send('ok')
     } catch (error) {
